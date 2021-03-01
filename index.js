bb0a2c35b168199ae1d8ba247c22d7560540c571
Sophie Engle <sjengle@cs.usfca.edu>
2021-03-01 13:47:10 -0800

    Turning off warnings

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ async function run() {
     core.startGroup('Compiling project main code...');
 
     status.mainCompile = await utils.checkExec('mvn', {
-      param: ['-ntp', '-DcompileOptionXlint=-Xlint:all', '-DcompileOptionXdoclint=-Xdoclint:all/private', '-Dmaven.compiler.showWarnings=true', '-DcompileOptionFail=false', '-Dmaven.compiler.failOnWarning=false', 'compile'],
+      param: ['-ntp', '-DcompileOptionXlint=-Xlint:none', '-DcompileOptionXdoclint=-Xdoclint:none', '-Dmaven.compiler.showWarnings=false', '-DcompileOptionFail=false', '-Dmaven.compiler.failOnWarning=false', 'compile'],
       title: 'Compiling project main code',
       error: 'Compiling returned non-zero exit code',
       chdir: `${utils.mainDir}/`
@@ -80,7 +80,7 @@ async function run() {
     core.startGroup('Compiling project test code...');
 
     status.testCompile = await utils.checkExec('mvn', {
-      param: ['-ntp', '"-DcompileOptionXlint=-Xlint:none"', '"-DcompileOptionXdoclint=-Xdoclint:none"', '-DcompileOptionFail=false', '-Dmaven.compiler.failOnWarning=false', '-Dmaven.compiler.showWarnings=true', 'test-compile'],
+      param: ['-ntp', '"-DcompileOptionXlint=-Xlint:none"', '"-DcompileOptionXdoclint=-Xdoclint:none"', '-DcompileOptionFail=false', '-Dmaven.compiler.failOnWarning=false', '-Dmaven.compiler.showWarnings=false', 'test-compile'],
       title: 'Compiling project test code',
       error: 'Compiling returned non-zero exit code',
       chdir: `${utils.mainDir}/`
